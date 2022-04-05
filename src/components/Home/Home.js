babd25c6d8acfefa893dedba0e46c4bc0adbcef9
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-04-05 12:51:17 +0600

    home page on add mapp

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Reviews from '../Reviews/Reviews';
 import './Home.css';
 const Home = () => {
+    const [reveiws,setReviews]=useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+    },[])
     return (
-        <div className='grid container'>
+        <div>
+          <div className="grid container">
           <div>
           <h1>You can get here the best healthy oil. </h1>
             <p>Our oil is healthy. It is completely free from adulteration. Anyone can take our oil without fear. 
@@ -12,6 +20,14 @@ const Home = () => {
                 <div className='picture'>
                 <img src="https://hatureya.com/wp-content/uploads/2019/04/36-1000x1000.jpg" alt="" />
                 </div>
+          </div>
+          <h1>Coustomer Reviews {reveiws.length}</h1>
+          {
+              reveiws.map(reveiw=><Reviews
+              reveiw={reveiw}
+              ></Reviews>)
+          }
+          <h4>name{reveiws.name}</h4>
         </div>
     );
 };
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const Reviews = () => {
     return (
         <div>
-            <h1>reviews</h1>
+            <h1>What our coustomers say ?</h1>
+            <Card>
+                <Card.Body></Card.Body>
+            </Card>
         </div>
     );
 };
