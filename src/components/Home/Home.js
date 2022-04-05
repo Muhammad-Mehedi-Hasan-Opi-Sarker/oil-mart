f2779002da6b921c712d7bf9895c0ab6521f40c2
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-04-05 11:54:14 +0600

    navbar home in set picture

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import './Home.css';
 const Home = () => {
     return (
-        <div>
+        <div className='grid container'>
+          <div>
           <h1>You can get here the best healthy oil. </h1>
             <p>Our oil is healthy. It is completely free from adulteration. Anyone can take our oil without fear. 
                 We have many buyers who regularly purchase our products. 
                 Take a look at our products. We also save on oil prices</p>
+          </div>
+                <div className='picture'>
+                <img src="https://hatureya.com/wp-content/uploads/2019/04/36-1000x1000.jpg" alt="" />
+                </div>
         </div>
     );
 };
